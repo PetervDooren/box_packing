5bbbcfe065cbbf493c8234a4758dd7e42e997aed
PetervDooren <PetervDooren@outlook.com>
2023-02-13 13:36:30 +0100

    add setup file

---
+++ setup.bash
@@ -0,0 +1,5 @@
+WORKSPACE=~/flexcraft_workspace
+source $WORKSPACE/devel/setup.bash
+
+export ED_MODEL_PATH=$WORKSPACE/src/box_packing/my_ed_object_models/models
+export GAZEBO_MODEL_PATH=$WORKSPACE/src/box_packing/my_ed_object_models/models
